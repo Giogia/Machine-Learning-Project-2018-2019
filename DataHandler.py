ab8ab32f990e322581a869868a980792328cecb3
giogia <Giovannitommasi95@gmail.com>
2019-04-04 11:14:49 +0800

    PEP8

--- DataHandler.py
+++ DataHandler.py
@@ -4,12 +4,15 @@ from Sets import Sets
 
 
 def randomize(a, b):
+
     # Generate the permutation index array.
     s = np.arange(a.shape[0])
     np.random.shuffle(s)
+
     # Shuffle the arrays by giving the permutation in the square brackets.
     shuffled_a = a[s]
     shuffled_b = b[s]
+
     return shuffled_a, shuffled_b
 
 
@@ -26,4 +29,5 @@ def load_data():
     sets = Sets(train_x[limit:], train_y[limit:], train_x[:limit], train_y[:limit], test_x, test_y)
     class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
     return sets, class_names
--- HyperparameterTuning.py
+++ HyperparameterTuning.py
@@ -2,13 +2,14 @@ from FeaturesSelector import FeaturesSelector
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
-#set first number of feature selected
+# set first number of feature selected
 START = 1
 
-#set number of feature increment at each iterations
+# set number of feature increment at each iterations
 STEP = 10
 
-def tune(classifier,selector,sets):
+
+def tune(classifier, selector, sets):
 
     training_error = []
     validation_error = []
@@ -17,33 +18,33 @@ def tune(classifier,selector,sets):
 
     while i < sets.x.size:
 
-        #extract reduced features
-        feature_selector = FeaturesSelector(selector,i)
+        # extract reduced features
+        feature_selector = FeaturesSelector(selector, i)
         reduced_sets = feature_selector.fit(sets)
 
-        #fit the classifier with reduced features
+        # fit the classifier with reduced features
         classifier.fit(reduced_sets.train.x, reduced_sets.train.y)
 
         training_predictions = classifier.predict(reduced_sets.train.x)
         validation_predictions = classifier.predict(reduced_sets.eval.x)
 
-        #add errors results to array
+        # add errors results to array
         training_error.append(1 - accuracy_score(training_predictions, reduced_sets.train.y))
         validation_error.append(1 - accuracy_score(validation_predictions, reduced_sets.eval.y))
 
         print(i, "Training error" + str(1 - accuracy_score(training_predictions, reduced_sets.train.y)),
-                "Validation error" + str(1 - accuracy_score(validation_predictions, reduced_sets.eval.y)))
+                 "Validation error" + str(1 - accuracy_score(validation_predictions, reduced_sets.eval.y)))
 
-        #save best number of features approximated to step resolution (real value with step=1)
+        # save best number of features approximated to step resolution (real value with step=1)
         best_features_number = STEP * validation_error.index(min(validation_error))
 
         i += STEP
 
         print("Minimum error for " + str(best_features_number) + " features")
 
-    plt.plot(range(0,sets.x.size-1,STEP),validation_error)
-    plt.plot(range(0,sets.x.size-1,STEP),training_error)
-
+    plt.plot(range(0, sets.x.size-1, STEP), validation_error)
+    plt.plot(range(0, sets.x.size-1, STEP), training_error)
     plt.show
 
     return best_features_number
+
