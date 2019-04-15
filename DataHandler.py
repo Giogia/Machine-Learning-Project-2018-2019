73e52477540897123ee7ef7199c19129e3e059cc
Andrea Perego <andrea14.perego@mail.polimi.it>
2019-04-15 15:55:45 +0800

    Fix in load_data for handling fit function of scikit-learn and introduced eval_percentage as parameter
    Signed-off-by: Andrea Perego <andrea14.perego@mail.polimi.it>

--- DataHandler.py
+++ DataHandler.py
@@ -16,16 +16,19 @@ def randomize(a, b):
     return shuffled_a, shuffled_b
 
 
-def load_data():
+def load_data(eval_percentage=0.2, linearized=True):
     fashion_mnist = keras.datasets.fashion_mnist
 
     (train_x, train_y), (test_x, test_y) = fashion_mnist.load_data()
+
+    if linearized:
+        train_x = train_x.reshape((-1, 784))
 
     train_x, train_y = randomize(train_x, train_y)
     train_x = train_x / 255.0
     test_x = test_x / 255.0
 
-    limit = int(len(train_x)*0.2)
+    limit = int(len(train_x)*eval_percentage)
     sets = Sets(train_x[limit:], train_y[limit:], train_x[:limit], train_y[:limit], test_x, test_y)
     class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
