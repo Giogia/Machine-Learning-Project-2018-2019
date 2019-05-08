f5b850c4ab008aa3f9fe8a8098dba8e4cc850413
giogia <Giovannitommasi95@gmail.com>
2019-05-08 14:47:11 +0800

    PEP8

--- number_feature_exploration.py
+++ number_feature_exploration.py
@@ -17,28 +17,28 @@ NUM_ATTEMPTS = 5
 
 # The default configuration of the parameters for the logistic regression
 lor_dict = {
-    'penalty':'l2',         # 'l1' or 'l2'
-    'dual':False,           # True if #feature > #samples (only if l2 active)
-    'tol':1e-4,             # tollerance for early stopping
-    'C':1.0,                # inverse 8of the regularization term of LoR smaller values implies stronger regularization
-    'fit_intercept':True,   # True if we want the Bias
-    'intercept_scaling':1,  # Useful only when solver 'liblinear'.
-                            # The higher it is, the less the bias are regularized, the bigger they can become
-    'class_weight':None,    # None,'balanced' or dict. For giving a weight to the various classes.
-                            # Usefull if unbalanced datsets.
-    'random_state':None,    # Seed for initializing the random generator --> for experiments reproducibility
-    'solver':'warn',        # 'newton-cg','lbfgs','liblinear','sag','saga'.
-                            # 'liblinear' good for small datasets.
-                            # 'sag' and 'saga' are faster on big datasets.
-                            # 'liblinear' has a one-vs-rest approach, the others use multinnomials.
-                            # 'newton-cg', 'lbfgs' and 'sag' use just L2 regularization.
-                            # 'sag' and 'saga' guarantee a fast convergence if the features are approximated on the same scale.
-    'max_iter':100,         # maximum number of iterations
-    'multi_class':'auto',   # 'ovr', 'multinomial' or 'auto'. We should use multinomial.
-                            # If we use liblinear solver, we have to use ovr.
-    'verbose':1,            # 0,1 o 2. Levels of verbosity.
-    'warm_start':False,     # It True it reuse the solution of previous fit.
-    'n_jobs':None           # Number of processors used by the computation.
+    'penalty': 'l2',  # 'l1' or 'l2'
+    'dual': False,  # True if #feature > #samples (only if l2 active)
+    'tol': 1e-4,  # tollerance for early stopping
+    'C': 1.0,  # inverse 8of the regularization term of LoR smaller values implies stronger regularization
+    'fit_intercept': True,  # True if we want the Bias
+    'intercept_scaling': 1,  # Useful only when solver 'liblinear'.
+    # The higher it is, the less the bias are regularized, the bigger they can become
+    'class_weight': None,  # None,'balanced' or dict. For giving a weight to the various classes.
+    # Usefull if unbalanced datsets.
+    'random_state': None,  # Seed for initializing the random generator --> for experiments reproducibility
+    'solver': 'warn',  # 'newton-cg','lbfgs','liblinear','sag','saga'.
+    # 'liblinear' good for small datasets.
+    # 'sag' and 'saga' are faster on big datasets.
+    # 'liblinear' has a one-vs-rest approach, the others use multinnomials.
+    # 'newton-cg', 'lbfgs' and 'sag' use just L2 regularization.
+    # 'sag' and 'saga' guarantee a fast convergence if the features are approximated on the same scale.
+    'max_iter': 100,  # maximum number of iterations
+    'multi_class': 'auto',  # 'ovr', 'multinomial' or 'auto'. We should use multinomial.
+    # If we use liblinear solver, we have to use ovr.
+    'verbose': 1,  # 0,1 o 2. Levels of verbosity.
+    'warm_start': False,  # It True it reuse the solution of previous fit.
+    'n_jobs': None  # Number of processors used by the computation.
 }
 
 # The parameters of the logistic regression that are modified from the default value
@@ -50,16 +50,16 @@ lor_dict['n_jobs'] = 1
 
 # The default configuration of the parameters for the gaussian naive bayes
 gnb_dict = {
-    'priors': None,          # Array of dimension equal to the number of classes.
-                             # It contains the prior distributions of the classes.
-    'var_smoothing': 1e-9    # Do not touch :)
+    'priors': None,  # Array of dimension equal to the number of classes.
+    # It contains the prior distributions of the classes.
+    'var_smoothing': 1e-9  # Do not touch :)
 }
 
 # If more methods are added, let's add it here
 feature_selector_methods = [FeaturesSelector.NO_REDUCTION, FeaturesSelector.PCA, FeaturesSelector.LDA]
 # feature_selector_methods = [FeaturesSelector.LDA]
 # classification_methods = [(Classifier.LOGISTIC,lor_dict), (Classifier.GAUSSIAN_NAIVE_BAYES,gnb_dict)]
-classification_methods = [(Classifier.GAUSSIAN_NAIVE_BAYES,gnb_dict)]
+classification_methods = [(Classifier.GAUSSIAN_NAIVE_BAYES, gnb_dict)]
 
 ################################################################################
 #################################### SCRIPT ####################################
@@ -80,30 +80,33 @@ for cl_method in classification_methods:
         with open(log_file_name, 'w') as log:
             # Creating the file and set the column names
             log.write("NumFeature;TrainingAccuracy;ValidationAccuracy;TestAccuracy\n")
-        
+
         for nf in number_of_features:
-            print("Method: {} \tNumber Feature: {}".format(fs_method,nf))
+            print("Method: {} \tNumber Feature: {}".format(fs_method, nf))
             accuracies = {'train': 0, 'eval': 0, 'test': 0}
             for _ in range(NUM_ATTEMPTS):
                 sets, class_names = load_data(linearized=True)
-                classifier = Classifier(cl_method[0],**cl_method[1])
-                selector = FeaturesSelector(fs_method,nf)
+                classifier = Classifier(cl_method[0], **cl_method[1])
+                selector = FeaturesSelector(fs_method, nf)
                 sets = selector.fit(sets)
-                train_predict, eval_predict = classifier.get_predictions(features=sets.train.x,
-                                                                         labels=sets.train.y,
-                                                                         eval_features=sets.eval.x,
-                                                                         eval_labels=sets.eval.y,
-                                                                         test_features=sets.test.x)
+                train_predict, eval_predict, test_predict = classifier.get_predictions(features=sets.train.x,
+                                                                                       labels=sets.train.y,
+                                                                                       eval_features=sets.eval.x,
+                                                                                       eval_labels=sets.eval.y,
+                                                                                       test_features=sets.test.x)
 
-                test_predict = classifier.classifier.predict(sets.test.x)
+                accuracies['train'] = accuracies['train'] + sum(
+                    [train_predict[i] == sets.train.y[i] for i in range(len(train_predict))]) / len(train_predict)
+                accuracies['eval'] = accuracies['eval'] + sum(
+                    [eval_predict[i] == sets.eval.y[i] for i in range(len(eval_predict))]) / len(eval_predict)
+                accuracies['test'] = accuracies['test'] + sum(
+                    [test_predict[i] == sets.test.y[i] for i in range(len(test_predict))]) / len(test_predict)
 
-                accuracies['train'] = accuracies['train'] + sum([train_predict[i] == sets.train.y[i] for i in range(len(train_predict))])/len(train_predict)
-                accuracies['eval'] = accuracies['eval'] + sum([eval_predict[i] == sets.eval.y[i] for i in range(len(eval_predict))])/len(eval_predict)
-                accuracies['test'] = accuracies['test'] + sum([test_predict[i] == sets.test.y[i] for i in range(len(test_predict))])/len(test_predict)
-            
             accuracies['train'] = accuracies['train'] / NUM_ATTEMPTS
             accuracies['eval'] = accuracies['eval'] / NUM_ATTEMPTS
             accuracies['test'] = accuracies['test'] / NUM_ATTEMPTS
 
             with open(log_file_name, 'a') as log:
-                log.write("{};{:.4};{:.4};{:.4}\n".format(sets.train.x.shape[1],accuracies['train'],accuracies['eval'],accuracies['test']))
+                log.write(
+                    "{};{:.4};{:.4};{:.4}\n".format(sets.train.x.shape[1], accuracies['train'], accuracies['eval'],
+                                                    accuracies['test']))
