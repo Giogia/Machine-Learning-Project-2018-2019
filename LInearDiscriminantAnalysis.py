1006fceffb6c276799cc52382090d8d8514fb297
giogia <Giovannitommasi95@gmail.com>
2019-05-20 15:27:30 +0800

    updated hyperparameter tuning

--- LInearDiscriminantAnalysis.py
+++ LInearDiscriminantAnalysis.py
@@ -1,6 +1,5 @@
 from DataHandler import load_data
 from CNN import CNN
-from HyperparameterTuning import tune
 from FeaturesSelector import FeaturesSelector
 from Classifier import Classifier
 
@@ -27,6 +26,8 @@ lda_dict = {
     'tol': 0.0001,
 }
 
+OPTIMAL_FEATURE_NUMBER = 100
+
 # Linearized must be True if not using CNN otherwise False
 sets, class_names = load_data(eval_percentage=0.2, linearized=False)
 
@@ -39,31 +40,8 @@ sets.train.x, sets.eval.x, sets.test.x = feature_extractor.extract(sets.train.x,
 # Create Classifier
 classifier = Classifier('lda', **lda_dict)
 
-# Tune feature selector parameter, disable feature selector prints for visually
-optimal_features_number_pca = tune(classifier, 'pca', sets)
-optimal_features_number_ica = tune(classifier, 'ica', sets)
-
 # Create feature selector
-feature_selector = FeaturesSelector('pca', optimal_features_number_pca)
-
-# Predict the training, evaluation and test set
-train_predict, eval_predict, test_predict = classifier.get_predictions(features=sets.train.x,
-                                                                       labels=sets.train.y,
-                                                                       eval_features=sets.eval.x,
-                                                                       eval_labels=sets.eval.y,
-                                                                       test_features=sets.test.x)
-
-train_accuracy = sum([train_predict[i] == sets.train.y[i] for i in range(len(train_predict))]) / len(train_predict)
-eval_accuracy = sum([eval_predict[i] == sets.eval.y[i] for i in range(len(eval_predict))]) / len(eval_predict)
-test_accuracy = sum([test_predict[i] == sets.test.y[i] for i in range(len(test_predict))]) / len(test_predict)
-
-print("\nTrain Accuracy: {}".format(train_accuracy))
-print("Validation Accuracy: {}".format(eval_accuracy))
-print("Test Accuracy: {}".format(test_accuracy))
-
-
-# Create feature selector
-feature_selector = FeaturesSelector('pca', optimal_features_number_ica)
+feature_selector = FeaturesSelector('pca', OPTIMAL_FEATURE_NUMBER)
 
 # Predict the training, evaluation and test set
 train_predict, eval_predict, test_predict = classifier.get_predictions(features=sets.train.x,
